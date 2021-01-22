1218287b8054ee60d441216d846f26cf6db79c0e
Felix Herbst <herbst@prefrontalcortex.de>
2021-01-22 22:49:05 +0100

    the best node in the world

---
+++ package/Editor/Internal/ShaderGraph/GenerateVertexIDNode.cs
@@ -0,0 +1,35 @@
+#if !NO_INTERNALS_ACCESS
+
+using UnityEditor.Graphing;
+using UnityEditor.ShaderGraph.Internal;
+
+namespace UnityEditor.ShaderGraph
+{
+    [Title("Input", "Custom", "Vertex ID Generator")]
+    internal class GenerateVertexIDNode : AbstractMaterialNode, IMayRequireVertexID, IGeneratesBodyCode
+    {
+        const string kOutputSlotName = "Out";
+        public const int OutputSlotId = 0;
+        
+        public GenerateVertexIDNode()
+        {
+            name = "Generate Vertex ID";
+            UpdateNodeAfterDeserialization();
+        }
+        
+        public sealed override void UpdateNodeAfterDeserialization()
+        {
+            AddSlot(new Vector1MaterialSlot(OutputSlotId, kOutputSlotName, kOutputSlotName, SlotType.Output, 0));
+            RemoveSlotsNameNotMatching(new[] { OutputSlotId });
+        }
+        
+        public bool RequiresVertexID(ShaderStageCapability stageCapability = ShaderStageCapability.All) => true;
+        
+        public void GenerateNodeCode(ShaderStringBuilder sb, GenerationMode generationMode)
+        {
+            sb.AppendLine(string.Format("$precision {0} = {1};", GetVariableNameForSlot(OutputSlotId), "IN.VertexID"));
+        }
+    }
+}
+
+#endif
