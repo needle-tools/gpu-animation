b5f71ad66cba618ac5ae9fca4481ecd9d6ed7ce0
Marcel Wiessler <marcel@gaisterhand.de>
2021-01-23 16:27:48 +0100

    uav RT test

---
+++ package/Runtime/Experimental/IndexTexture.shader
@@ -0,0 +1,40 @@
+﻿Shader "Custom/IndexTexture"
+{
+	Properties
+	{
+	}
+	SubShader
+	{
+		Tags
+		{
+			"RenderType"="Opaque"
+		}
+
+		CGPROGRAM
+		#pragma surface surf Standard fullforwardshadows
+		#pragma target 5.0
+		
+
+		#if defined(SHADER_API_D3D11) || defined(SHADER_API_METAL)
+		uniform RWTexture2D<float4> _MyTex : register(u4); 
+		#endif
+
+
+		struct Input
+		{
+			float2 uv_MainTex;
+		};
+
+		void surf(Input IN, inout SurfaceOutputStandard o)
+		{
+			#if defined(SHADER_API_D3D11) || defined(SHADER_API_METAL)
+			for (int x = 0; x < 5; x++)
+				_MyTex[int2(x,x)] = 1;// .5 + sin(_Time.y) * .5 + .5;
+			#endif
+
+			o.Albedo = 1;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
---
+++ package/Runtime/Experimental/RenderWithIndexTexture.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.Graphs;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+namespace Experimental
+{
+	[ExecuteAlways]
+	public class RenderWithIndexTexture : MonoBehaviour
+	{
+		public Camera Cam;
+		public Mesh Mesh;
+		public Material Material;
+		public Material Output;
+
+		private RenderTexture rt;
+
+		private void OnEnable()
+		{
+			Camera.onPreCull += OnBeforeRender;
+		}
+		
+		private void OnDisable()
+		{
+			Camera.onPreCull -= OnBeforeRender;
+			rt.Release();
+		}
+
+		// private void OnRenderImage(RenderTexture src, RenderTexture dest)
+		// {
+		// 	Execute();
+		// 	Graphics.Blit(src, dest);
+		// }
+
+		private void OnBeforeRender(Camera cam)
+		{
+			Debug.Log("Render " + cam);// + " - " + Time.frameCount, cam); 
+			Execute(cam);
+		}
+
+		// private void Update()
+		// {
+		// 	Execute();
+		// }
+
+		private void Execute(Camera cam)
+		{
+			if (!rt)
+			{
+				rt = new RenderTexture(5, 5, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+				rt.enableRandomWrite = true;
+				rt.name = "MyTex";
+				rt.filterMode = FilterMode.Point;
+				rt.Create();
+				Shader.SetGlobalTexture("_MyTex", rt);
+				Graphics.ClearRandomWriteTargets(); 
+				Graphics.SetRandomWriteTarget(4, rt);;
+			}
+			
+			Output.mainTexture = rt;
+			Material.SetTexture("_MyTex", rt);
+			
+			Graphics.SetRandomWriteTarget(4, rt);
+			// Material.enableInstancing = true;
+			// Graphics.DrawMeshInstanced(Mesh, 0, Material, new []{transform.localToWorldMatrix});
+			Graphics.ClearRandomWriteTargets();
+			
+			Cam.RemoveCommandBuffers(CameraEvent.BeforeForwardOpaque);
+			Cam.RemoveCommandBuffers(CameraEvent.BeforeForwardOpaque);
+			var cmd = new CommandBuffer();
+			cmd.ClearRandomWriteTargets();
+			cmd.SetRandomWriteTarget(4, new RenderTargetIdentifier(rt));
+			Cam.AddCommandBuffer(CameraEvent.BeforeForwardOpaque, cmd);
+		
+		}
+
+		// ()
+		// {
+		// }
+	}
+}
